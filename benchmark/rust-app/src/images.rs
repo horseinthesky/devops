ba3d496201b56de60e5c0a1156bb946f2b735aeb
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2024-02-11 19:36:47 +0300

    rust-app image struct

--- benchmark/rust-app/src/images.rs
+++ benchmark/rust-app/src/images.rs
@@ -1,14 +1,33 @@
 use tokio::time::{sleep, Duration};
+use std::time::SystemTime;
+use uuid::Uuid;
+
+#[allow(dead_code)]
+struct Image {
+    uuid: Uuid,
+    modified: SystemTime,
+}
+
+impl Image {
+    fn new() -> Self {
+        Image {
+            uuid: Uuid::new_v4(),
+            modified: SystemTime::now(),
+        }
+    }
+}
 
 async fn download() {
     sleep(Duration::from_millis(5)).await;
 }
 
-async fn save() {
+async fn save(_image: Image) {
     sleep(Duration::from_millis(2)).await;
 }
 
 pub async fn get_images() {
     download().await;
-    save().await;
+
+    let image = Image::new();
+    save(image).await;
 }
